02115371f8c80ebabb624ff53daab2583cb41bd5
Matthew Lowery <mattlowery@Matthews-MBP-2.AN-510-AP-I-AC>
2022-04-25 15:31:30 +0100

    ~

--- src/utils.py
+++ src/utils.py
@@ -50,7 +50,8 @@ def mean_squared_error(orig, pred, mask, error_bars=1.):
     print(error_bars.shape, pred.shape, mask.shape, orig.shape)
     print(error_bars[0,0,0], pred[0,0,0,0], mask[0,0,0], orig[0,0,0])
     error = ((orig - pred) / error_bars) ** 2
-
+    print(error)
+    # need 0.0000000000001 in values to not divide by zero 
 
     #error = (orig - pred) ** 2
     

3b7d266307c948b5ddfbfb7e8dbf96673c9034e2
Matthew Lowery <mattlowery@matthews-mbp-2.wlan.dur.ac.uk>
2022-04-26 16:46:12 +0100

    diving by zero and replacing the zero was breaking autograd

--- src/utils.py
+++ src/utils.py
@@ -64,24 +64,13 @@ def resample_lcs(lcs):
 
 def mean_squared_error(orig, pred, mask, error_bars=1.):
     error_bars = error_bars + 0.000001
-    # the higher the error bars, the lower the loss
-    #new_error = ((orig - pred) / error_bars) ** 2
     new_error = ((orig - pred)**2) / (error_bars**2)
     error = (orig - pred) ** 2
-    # shape is 1,8,267,1
-    # zero divide mess up back prop?
-    #new_error = torch.nan_to_num(new_error, posinf=0.0)
-
     error = error * mask
     new_error = new_error * mask
     mask_sum = mask.sum()
-
     old_return = error.sum() / mask_sum
     new_return = new_error.sum() / mask_sum
-    print(old_return, new_return)
-    #print(new_error.sum() / mask_sum, 'now')
-    # need 0.0000000000001 in values to not divide by zero 
-
     return new_return
 
 

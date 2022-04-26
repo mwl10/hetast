5a137f468b6bc22958b70f6422ae832ea1457dbc
Matthew Lowery <mattlowery@matthews-mbp-2.wlan.dur.ac.uk>
2022-04-26 15:57:28 +0100

    check original?

--- src/utils.py
+++ src/utils.py
@@ -66,22 +66,22 @@ def mean_squared_error(orig, pred, mask, error_bars=1.):
 
     # the higher the error bars, the lower the loss
     #new_error = ((orig - pred) / error_bars) ** 2
-    new_error = ((orig - pred)**2) / (error_bars**2)
+    #new_error = ((orig - pred)**2) / (error_bars**2)
     error = (orig - pred) ** 2
     # shape is 1,8,267,1
-    new_error = torch.nan_to_num(new_error, posinf=0.0)
+    #new_error = torch.nan_to_num(new_error, posinf=0.0)
 
     error = error * mask
-    new_error = new_error * mask
+    #new_error = new_error * mask
     
     print(error.sum() / mask.sum(), 'before')
-    print(new_error.sum() / mask.sum(), 'now')
+    #print(new_error.sum(), mask.sum(), 'now')
     # need 0.0000000000001 in values to not divide by zero 
 
     #error = (orig - pred) ** 2
     
     #error = error * mask
-    return new_error.sum() / mask.sum()
+    return error.sum() / mask.sum()
 
 
 def mean_absolute_error(orig, pred, mask):

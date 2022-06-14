4e6bc66e7678b1175a86bf041210c7bc258340b1
Matthew Lowery <mattlowery@Matthews-MacBook-Pro-2.local>
2022-06-14 17:44:20 -0400

    fix min&max for files_to_numpy

--- src/dataset.py
+++ src/dataset.py
@@ -20,6 +20,7 @@ class DataSet:
 
             if len(example) < minimum or len(example) > maximum:
                 del self.files[i]
+                pass
 
             print(f'dims of {file}:\t{example.shape}')
             example = example[example[:,0].argsort()]

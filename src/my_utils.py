362112358ac7aca744ca5919718f297065e7c6d3
Matthew Lowery <mattlowery@Matthews-MacBook-Pro-2.local>
2022-06-17 10:53:09 -0400

    bugg

--- src/my_utils.py
+++ src/my_utils.py
@@ -13,11 +13,12 @@ def make_masks(lcs, frac=0.7):
     subsampled_mask = torch.zeros_like(lcs[:, :, 1])
     recon_mask = torch.zeros_like(lcs[:, :, 1])
     for i, lc in enumerate(lcs):
-        indexes = lc[:, 1].nonzero()[0]
+        indexes = lc[:, 1].nonzero()
         # this should vary to some extent
         length = int(np.round(len(indexes) * frac))
         obs_points = np.sort(np.random.choice(
             indexes, size=length, replace=False))
+        
         subsampled_mask[i, obs_points] = 1
 
         recon_mask[i] = torch.logical_xor(lc[:, 1], subsampled_mask[i])

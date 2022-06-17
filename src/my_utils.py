30ef755de7211a35b4c5353bb242245fa4beb6c0
Matthew Lowery <mattlowery@Matthews-MacBook-Pro-2.local>
2022-06-17 10:56:38 -0400

    fixo

--- src/my_utils.py
+++ src/my_utils.py
@@ -13,7 +13,7 @@ def make_masks(lcs, frac=0.7):
     subsampled_mask = torch.zeros_like(lcs[:, :, 1])
     recon_mask = torch.zeros_like(lcs[:, :, 1])
     for i, lc in enumerate(lcs):
-        indexes = lc[:, 1].nonzero()
+        indexes = lc[:, 1].nonzero()[:,0]
         # this should vary to some extent
         length = int(np.round(len(indexes) * frac))
         obs_points = np.sort(np.random.choice(

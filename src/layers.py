ebc3a07d6cbf42a55a5be29ebf77e3b5bed94126
Matthew Lowery <mattlowery@Matthews-MacBook-Pro-2.local>
2022-06-26 12:45:56 +0200

    try to get attn vectors

--- src/layers.py
+++ src/layers.py
@@ -87,11 +87,10 @@ class UnTAN(nn.Module):
             scores = scores.masked_fill(mask.unsqueeze(-3) == 0, -1e9)
         p_attn = F.softmax(scores, dim=-2) # these are the probability scores for attention
         #print("attention probs", p_attn.shape)
-        # np.savetxt(f'./p_attn/{p_attn.shape}',p_attn.cpu())
-        # np.savetxt(f'./query/{query.shape}', query.cpu())
-        # np.savetxt(f'./key/{key.shape}', key.cpu())
+        np.savetxt(f'./p_attn/{p_attn.shape}',p_attn.detach().clone())
+        np.savetxt(f'./query/{query.shape}', query.detach().clone())
+        np.savetxt(f'./key/{key.shape}', key.detach().clone())
         
-        print(p_attn.shape)
         if dropout is not None:
             p_attn = dropout(p_attn)
         if self.intensity:

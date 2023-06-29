43e79472bb0571ce5872ff93ddb77ecf5dd3ab5b
mwl10 <mattlowery10@gmail.com>
2023-06-29 14:05:09 -0600

    mcg roa stuff

--- src/pyroa_mcg_intrp.py
+++ src/pyroa_mcg_intrp.py
@@ -12,17 +12,16 @@ default = [[0.0, 10.0], [0, 30.0], [0.00, 50.0], [0.01, 10.0], [0.0, 10.0]]
 med = [[0.0, 10.0], [0, 30.0], [0.00, 50.0], [5,15], [0.0, 10.0]]
 high = [[0.0, 10.0], [0.0, 30.0], [0.00, 50.0], [10,30], [0.0, 10.0]]
 highest = [[0.0, 10.0], [0.0, 30.0], [0.00, 50.0], [20,30], [0.0, 10.0]]
-init_deltas = [1,10,20,25]
+init_deltas = [10,20,25]
 
 if not os.path.isdir('pyroa_fits'): 
     os.mkdir('pyroa_fits')
   
 
-segments = ['notebooks/intrps/MCG+08-11-011/gri_seg12_10_det','notebooks/intrps/MCG+08-11-011/gri_seg1_10_det', 'notebooks/intrps/MCG+08-11-011/gri_seg2_10_det',
-           'notebooks/intrps/MCG+08-11-011/g,r_finet_seg12_det','notebooks/intrps/MCG+08-11-011/gri_seg2_5_det']
+segments = ['notebooks/intrps/MCG+08-11-011/gri_seg12_10_det','notebooks/intrps/MCG+08-11-011/gri_seg1_10_det', 'notebooks/intrps/MCG+08-11-011/gri_seg2_10_det']
 
 
-for i, prior in enumerate([default,med,high,highest]):
+for i, prior in enumerate([med,high,highest]):
     for segment in segments:
         fit = PyROA.Fit(segment + '/', obj_name, filters, prior, Nburnin=20000,Nsamples=50000, delay_dist=True,
                         add_var=True, psi_types=psi_types,init_delta=init_deltas[i])
